8cbe86787a9a84257f0493240919f42dd82ac7a5
Marco Risi <marco.risi.dev@gmail.com>
2024-04-22 12:01:05 +0200

    Show some debug info when opening the mobile app

--- mobile-app/App.js
+++ mobile-app/App.js
@@ -1,11 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, Image, StyleSheet, Text, View } from "react-native";
+
+import { useShareIntent, ShareIntentFile } from "expo-share-intent";
+import { Fragment } from "react";
 
 export default function App() {
+  const { hasShareIntent, shareIntent, resetShareIntent, error } =
+    useShareIntent({
+      debug: true,
+      resetOnBackground: true,
+    });
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image
+        source={require("./assets/icon.png")}
+        style={[styles.logo, styles.gap]}
+      />
+      <Text style={[styles.gap, styles.bold]}>
+        {hasShareIntent ? "SHARE INTENT FOUND !" : "NO SHARE INTENT DETECTED"}
+      </Text>
+
+      {/* TEXT and URL */}
+      {!!shareIntent.text && <Text style={styles.gap}>{shareIntent.text}</Text>}
+      {!!shareIntent.meta?.title && (
+        <Text style={styles.gap}>{JSON.stringify(shareIntent.meta)}</Text>
+      )}
+
+      {/* FILES */}
+      {shareIntent?.files?.map((file) => (
+        <Fragment key={file.path}>
+          {file.mimeType.startsWith("image/") && (
+            <Image source={{ uri: file.path }} style={[styles.image]} />
+          )}
+          <FileMeta file={file} />
+        </Fragment>
+      ))}
+
+      {/* FOOTER */}
+      {!!shareIntent && (
+        <Button onPress={() => resetShareIntent()} title="Reset" />
+      )}
+      <Text style={[styles.error]}>{error}</Text>
+    </View>
+  );
+}
+
+function FileMeta({ file }) {
+  return (
+    <View style={[styles.gap, styles.meta]}>
+      <Text style={styles.bold}>{file.fileName}</Text>
+      <Text>
+        {file.mimeType} ({Math.round((file.size || 0) / 1024)}
+        ko)
+      </Text>
+      {file.width && (
+        <Text>
+          {file.width} x {file.height}
+        </Text>
+      )}
     </View>
   );
 }
@@ -13,8 +65,33 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  logo: {
+    width: 75,
+    height: 75,
+    resizeMode: "contain",
+  },
+  image: {
+    width: 300,
+    height: 200,
+    resizeMode: "contain",
+    // backgroundColor: "lightgray",
+  },
+  gap: {
+    marginBottom: 20,
+  },
+  bold: {
+    fontWeight: "bold",
+  },
+  meta: {
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  error: {
+    color: "red",
+    marginTop: 20,
   },
 });
