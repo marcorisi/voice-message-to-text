2f77c19351deefb4d36f647396eca12bebc16af6
Marco Risi <marco.risi.dev@gmail.com>
2024-05-01 12:15:43 +0200

    Implement simple class to manage the DB

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from audio import Audio
+from db import DB
 
 app = Flask(__name__)
 
@@ -12,8 +13,13 @@ def transcribe():
     audio_file = request.files['audio']
     file_name = f"storage/{audio_file.filename}"
     audio_file.save(file_name)
+
     my_audio = Audio(file_name)
     result = my_audio.transcribe()
+
+    db = DB()
+    db.insert_audio(my_audio)
+
     response = jsonify(result)
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response, 200
---
+++ backend/db.py
@@ -0,0 +1,47 @@
+from tinydb import TinyDB, Query
+
+
+class DB:
+    """
+    A class representing a database.
+
+    Attributes:
+        db (TinyDB): The TinyDB instance representing the database.
+
+    Methods:
+        get(audio_id): Retrieve an audio record from the database based on its ID.
+        insert_audio(audio): Insert an audio record into the database.
+    """
+
+    def __init__(self, db_name='db.json'):
+        """
+        Initialize the DB class.
+
+        Creates a TinyDB instance and sets it as the database.
+        """
+        self.db = TinyDB(db_name)
+
+    def get(self, audio_id):
+        """
+        Retrieve an audio record from the database based on its ID.
+
+        Args:
+            audio_id (int): The ID of the audio record to retrieve.
+
+        Returns:
+            dict: The audio record matching the given ID.
+        """
+        Audio = Query()
+        return self.db.get(Audio.id == audio_id)
+
+    def insert_audio(self, audio):
+        """
+        Insert an audio record into the database.
+
+        Args:
+            audio (dict): The audio record to insert.
+
+        Returns:
+            int: The ID of the inserted audio record.
+        """
+        return self.db.insert(audio.to_dict())
