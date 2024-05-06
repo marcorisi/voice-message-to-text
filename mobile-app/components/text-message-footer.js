a5b934e871f54fa48c56e6cac12a0355084b1c59
Marco Risi <marco.risi.dev@gmail.com>
2024-05-06 12:44:32 +0200

    Show transcription type to footer

--- mobile-app/components/text-message-footer.js
+++ mobile-app/components/text-message-footer.js
@@ -1,10 +1,26 @@
 import { View, Text, StyleSheet } from 'react-native';
 
-export function TextMessageFooter({ messageId, messageLength }) {
+export function TextMessageFooter({ messageId, messageLength, responseStatusCode }) {
+
+    let transcriptionType = "";
+
+    switch (responseStatusCode) {
+        case 200:
+            transcriptionType = "Existing";
+            break;
+        case 201:
+            transcriptionType = "New";
+            break;
+        default:
+            transcriptionType = "Unknown";
+            break;
+    }
+
     return (
         <View style={ styles.container }>
             <Text style={ styles.text }>Message Id: {messageId}</Text>
             <Text style={ styles.text }>Length: {messageLength}</Text>
+            <Text style={ styles.text }>Transcription Type: {transcriptionType}</Text>
         </View>
     )
 }
