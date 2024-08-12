0564a717ab4fab7047b71ee5bf7981b44b6a9559
Marco Risi <marco.risi.dev@gmail.com>
2024-08-12 22:46:43 +0200

    Add show all

--- backend/scripts/db-helper.py
+++ backend/scripts/db-helper.py
@@ -11,10 +11,22 @@ def show_numbers_of_audio_stored_in_db(db: DB):
     print(f"Number of audio stored in the DB: {len(all_records)}\n")
     input("Press Enter to continue...")
 
+def show_all_items_stored_in_db(db: DB):
+    os.system('clear')
+    all_records = db.get_all()
+    print("Printing all the items stored in the DB.")
+    print("Press Enter to continue or x to exit.\n")
+    for record in all_records:
+        print(record)
+        key = input()
+        if key == 'x':
+            break
+
 def print_command_menu():
     os.system('clear')
     print("How can I help you?")
     print("(1) Show numbers of audio stored in the DB.")
+    print("(2) Show all the items stored in the DB.")
     print("(*) Exit")
 
 def main():
@@ -27,6 +39,8 @@ def main():
         match command:
             case '1':
                 show_numbers_of_audio_stored_in_db(db)
+            case '2':
+                show_all_items_stored_in_db(db)
             case _:
                 break
         
