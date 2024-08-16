593cac7830a33ff7a388b4b8cd6b5e340c295ab0
Marco Risi <marco.risi.dev@gmail.com>
2024-08-16 19:02:53 +0200

    Refactor decorator

--- backend/decorators.py
+++ backend/decorators.py
@@ -5,12 +5,16 @@ def authorize_request(function):
     A decorator that authorizes a request based on the presence of an API key in the request headers.
     """
     AUTHORIZATION_HEADER = 'X-API-KEY'
+
+    def error_response(message: str = None):
+        message = message or "Unauthorized"
+        response = jsonify({ "error" : message })
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        return response, 401
     
     def inner(*args, **kwargs):
         if AUTHORIZATION_HEADER not in request.headers:
-            response = jsonify({"error": "Unauthorized"})
-            response.headers.add('Access-Control-Allow-Origin', '*')
-            return response, 401
+            return error_response()
         
         return function(*args, **kwargs)
     
