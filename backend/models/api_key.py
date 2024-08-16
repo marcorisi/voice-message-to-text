3d0edc15d9b7eb6bba43e98cae82c0067f890f3e
Marco Risi <marco.risi.dev@gmail.com>
2024-08-16 15:37:41 +0200

    Api Key model

---
+++ backend/models/api_key.py
@@ -0,0 +1,39 @@
+import secrets
+
+class ApiKey:
+    """
+    Represents an api key object and provides methods for key generation.
+    """
+
+    @classmethod
+    def generate(cls, user):
+        """
+        Generates a new api key for the user.
+
+        Parameters:
+        - user (str): The user for whom the api key is generated.
+        """
+        api_key = cls()
+        api_key.key = secrets.token_urlsafe(32)
+        print(api_key.key)
+        api_key.user = user
+        return api_key
+
+    def __init__(self):
+        """
+        Initializes an Api Key object.
+        """
+        self.key = None
+        self.user = None
+    
+    def to_dict(self):
+        """
+        Converts the Api Key object to a dictionary that can be stored in the database.
+        The returned dictionary contains only a few relevant properties.
+
+        Returns:
+        - dict: A dictionary representation of the Api Key object.
+        """
+        PROPERTY_TO_KEEP = ["key", "user"]
+        dict = self.__dict__
+        return {key: dict[key] for key in PROPERTY_TO_KEEP}
---
+++ backend/tests/test_api_key.py
@@ -0,0 +1,31 @@
+import unittest
+from models.api_key import ApiKey
+
+class ApiKeyTests(unittest.TestCase):
+    def setUp(self):
+        self.user = "test_user"
+
+    def test_init(self):
+        obj = ApiKey()
+        self.assertIsNone(obj.key)
+        self.assertIsNone(obj.user)
+
+    def test_generate(self):
+        api_key = ApiKey.generate(self.user)
+        self.assertIsInstance(api_key, ApiKey)
+        self.assertIsNotNone(api_key.key)
+        self.assertEqual(api_key.user, self.user)
+
+    def test_generate_another_key_for_same_user(self):
+        api_key_1 = ApiKey.generate(self.user)
+        api_key_2 = ApiKey.generate(self.user)
+        self.assertIsNotNone(api_key_1.key)
+        self.assertIsNotNone(api_key_2.key)
+        self.assertNotEqual(api_key_1.key, api_key_2.key)
+
+    def test_to_dict(self):
+        api_key = ApiKey.generate(self.user)
+        api_key_dict = api_key.to_dict()
+        self.assertEqual(2, len(api_key_dict.keys()))
+        self.assertIn("key", api_key_dict)
+        self.assertIn("user", api_key_dict)
