bcdd535563f01e412997635d8f35744e4d163a69
Marco Risi <marco.risi.dev@gmail.com>
2024-08-20 22:38:33 +0200

    Restart gunicorn after deploy

--- backend/scripts/deploy.sh
+++ backend/scripts/deploy.sh
@@ -16,3 +16,8 @@ exit_if_error $? "Error pulling the latest changes from the repository"
 print_green "Installing the dependencies in the virtual environment"
 venv/bin/pip install -r requirements.txt
 exit_if_error $? "Error installing the dependencies in the virtual environment"
+
+# This will _probably_ fail if you have multiple gunicorn processes running
+print_green "Restarting the gunicorn server"
+kill -HUP `ps -C gunicorn fch -o pid | head -n 1`
+exit_if_error $? "Error restarting the gunicorn server"
